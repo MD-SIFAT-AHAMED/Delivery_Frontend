36a0355656a7c5ed89e28be28769f5971c69c54a
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-10-09 00:52:06 +0600

    feat(navbar): add responsive drawer menu for mobile view

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router";
+import ProFastLogo from "../../Pages/Shared/ProFastLogo/ProFastLogo";
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
+import { MdEmail, MdPhone } from "react-icons/md";
+import { IoLocationSharp } from "react-icons/io5";
 
 const Footer = () => {
-    return (
-        <div>
-            <h3>Footer</h3>
-        </div>
-    );
+  return (
+    <footer className="bg-gray-900 text-gray-300 pt-12 pb-6">
+      
+    </footer>
+  );
 };
 
 export default Footer;
--- src/Component/Navber/Navber.jsx
+++ src/Component/Navber/Navber.jsx
@@ -1,37 +1,84 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router";
 import ProFastLogo from "../../Pages/Shared/ProFastLogo/ProFastLogo";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navber = () => {
-  const [mobileMenu, setMobileMenu] = useState(false);
   const links = (
     <>
-      <li>Services</li>
+      <li>
+        <NavLink to={"/Services"}>Services</NavLink>
+      </li>
       <li>
         <NavLink to={"/coverage"}>Coverage</NavLink>
       </li>
       <li>
         <NavLink to={"/sendparcel"}>Send A Parcel</NavLink>
       </li>
-      <li>About Us</li>
-      <li>Pricing</li>
+      <li>
+        <NavLink to={"/AboutUs"}>About Us</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/Pricing"}>Pricing</NavLink>
+      </li>
       <li>
         <NavLink to={"/beARider"}>Be a Rider</NavLink>
       </li>
     </>
   );
+
   return (
-    <div className="bg-gray-300">
-      <div className="max-w-screen-2xl w-11/12 mx-auto bg-base-100">
-        <div className="flex justify-between items-center">
-          <div>
-            <ProFastLogo />
+    <>
+      {/* Drawer Wrapper */}
+      <div className="drawer">
+        {/* Hidden Checkbox */}
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+
+        {/* Drawer Content */}
+        <div className="drawer-content">
+          {/* Navbar */}
+          <div className="max-w-screen-2xl w-11/12 mx-auto bg-base-100 p-3 rounded-2xl">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center">
+                {/* Label linked with input */}
+                <label
+                  htmlFor="my-drawer"
+                  className="lg:hidden me-3 cursor-pointer"
+                >
+                  <RxHamburgerMenu size={25} />
+                </label>
+
+                <ProFastLogo />
+              </div>
+
+              <ul className="hidden lg:flex gap-4">{links}</ul>
+
+              <div className="flex gap-2">
+                <p className="bg-primary text-gray-800 font-medium px-3 py-2 rounded-xl">
+                  Sign Up
+                </p>
+                <p className="bg-primary text-gray-800 px-3 py-2 rounded-xl font-medium">
+                  Log In
+                </p>
+              </div>
+            </div>
           </div>
-          <ul className="flex gap-2">{links}</ul>
-          <div></div>
+        </div>
+
+        {/* Drawer Side (Sidebar Menu) */}
+        <div className="drawer-side overflow-hidden">
+          <label
+            htmlFor="my-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 space-y-2">
+            {links}
+          </ul>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
